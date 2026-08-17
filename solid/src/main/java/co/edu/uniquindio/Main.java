602e189eaac23c924a953874ef05b5be468c2957
Tapasco06 <juanjoseft06@gmail.com>
2026-08-16 19:30:57 -0500

    Agrego mensaje de juan jose

--- solid/src/main/java/co/edu/uniquindio/Main.java
+++ solid/src/main/java/co/edu/uniquindio/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-       
-        System.out.println("hola soy juan jose fernandez tapasco");
+
+        System.out.println("hola, soy juan jose fernandez tapasco estudiante de ingeieria");
 
     }
 }
