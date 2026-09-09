fa0d80afdc2f845bbc062a95c589985dcfcab2e6
Andressuarez-19 <andresf.suarezc@uqvirtual.edu.co>
2026-09-08 20:45:37 -0500

    Primer syso

--- solid/src/main/java/co/edu/uniquindio/Main.java
+++ solid/src/main/java/co/edu/uniquindio/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
 
         System.out.println("hola, soy juan jose fernandez tapasco estudiante de ingeieria");
+        System.out.println("hola, soy Andres Felipe Suarez Celis estudiante de ingeieria");
 
     }
 }
